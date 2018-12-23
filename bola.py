3214032fa28c836ee99c1ca3d548d435d25ce26c
vintan kirana <44189886+L200180144@users.noreply.github.com>
2018-12-23 21:17:04 +0700

    praktikum 12 kegiatan 2 dan 3

---
+++ bola.py
@@ -0,0 +1,26 @@
+def hitung_luas():
+    "fungsi menghitung PERSEGI"
+    s = 5
+    luas = s * s
+    return luas
+
+
+print "<!DOCTYPE html"
+print
+print """<html>
+<head>
+    <title> Bangung Geometri> </title>
+</head>"""
+
+print "<body> <table> <tr>"
+print "<td> <img src= 'persegi.JPG' width= '150' height= '150'> </td>"
+print "<td> <h1> BANGUN GEOMETRI </h1>"
+print "Nama Bangun : Persegi </br>"
+print "Dimensi : 2D </br>"
+print "Rumus Luas : s * s </br>"
+print "Sisi : 5 </br>"
+print "Luas : ", hitung_luas()
+print """ </td>
+</tr>
+</table>"""
+print "</body> </html>"
